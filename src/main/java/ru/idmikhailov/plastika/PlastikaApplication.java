71d642a0d7c472111a265fb2ee0edf37bccba458
IDMikhailov <idmikhailov@lanit.ru>
2019-11-29 17:21:52 +0300

    init commit for backend

---
+++ src/main/java/ru/idmikhailov/plastika/PlastikaApplication.java
@@ -0,0 +1,13 @@
+package ru.idmikhailov.plastika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlastikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlastikaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/idmikhailov/plastika/PlastikaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.idmikhailov.plastika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlastikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
